6b5add99194aceeb6b3798776b6a9f175ecde109
Ayden Le <soloyeti41@gmail.com>
2025-07-20 12:54:06 -0700

    Changing Screen Dimensions Co-authored-by: Easton Schwabe <EastonSch@users.noreply.github.com>

---
+++ src/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    fps: { forceSetTimeOut: true, target: 60 },   // ensure consistent timing across machines
+    width: 2000,
+    height: 1000,
+    scene: [Potato]
+}
+
+
+const game = new Phaser.Game(config);
