f01510196087eb0f1642ff25489b6fa488d93786
Easton Schwabe <eastonbs03@gmail.com>
2025-07-20 20:39:36 -0700

    Potato Movement

--- src/Scenes/Potato.js
+++ src/Scenes/Potato.js
@@ -30,6 +30,7 @@ class Potato extends Phaser.Scene {
         this.right = this.input.keyboard.addKey("D");
         this.up = this.input.keyboard.addKey("W");
         this.down = this.input.keyboard.addKey("S");
+        this.launch = this.input.keyboard.addKey("L");
         this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.shift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
 
@@ -50,6 +51,26 @@ class Potato extends Phaser.Scene {
                 { key: "leftmidwalk1" },
                 { key: "leftwalk2" },
                 { key: "leftwalk1" }
+            ],
+            frameRate: 6,
+            repeat: -1,
+            hideOnComplete: false
+        });
+               this.anims.create({
+            key: "walkUp",
+            frames: [
+                { key: "potato" },
+                { key: "idle" }
+            ],
+            frameRate: 6,
+            repeat: -1,
+            hideOnComplete: false
+        });
+        this.anims.create({
+            key: "walkDown",
+            frames: [
+                { key: "potato" },
+                { key: "idle" }
             ],
             frameRate: 6,
             repeat: -1,
@@ -85,11 +106,47 @@ class Potato extends Phaser.Scene {
             this.my.sprite.potato.setVelocityY(0);
             this.my.sprite.potato.play('walkLeft',true);
         }
+        //UP
+        else if(!this.left.isDown && !this.right.isDown && this.up.isDown && !this.down.isDown){
+            this.my.sprite.potato.setVelocityY(-200);
+            this.my.sprite.potato.setVelocityX(0);
+            this.my.sprite.potato.play('walkUp',true);
+        }
+        //DOWN
+        else if(!this.left.isDown && !this.right.isDown && !this.up.isDown && this.down.isDown){
+            this.my.sprite.potato.setVelocityY(200);
+            this.my.sprite.potato.setVelocityX(0);
+            this.my.sprite.potato.play('walkDown',true);
+        }
+        //LEFT UP
+        else if(this.left.isDown && !this.right.isDown && this.up.isDown && !this.down.isDown){
+            this.my.sprite.potato.setVelocityX(-180);
+            this.my.sprite.potato.setVelocityY(-180);
+            this.my.sprite.potato.play('walkLeft',true);
+        }
+        //RIGHT UP
+        else if(!this.left.isDown && this.right.isDown && this.up.isDown && !this.down.isDown){
+            this.my.sprite.potato.setVelocityX(180);
+            this.my.sprite.potato.setVelocityY(-180);
+            this.my.sprite.potato.play('walkRight',true);
+        }
+        //LEFT DOWN
+        else if(this.left.isDown && !this.right.isDown && !this.up.isDown && this.down.isDown){
+            this.my.sprite.potato.setVelocityX(-180);
+            this.my.sprite.potato.setVelocityY(180);
+            this.my.sprite.potato.play('walkLeft',true);
+        }
+        //RIGHT DOWN
+        else if(!this.left.isDown && this.right.isDown && !this.up.isDown && this.down.isDown){
+            this.my.sprite.potato.setVelocityX(180);
+            this.my.sprite.potato.setVelocityY(180);
+            this.my.sprite.potato.play('walkRight',true);
+        }
         else{
             this.my.sprite.potato.setVelocityX(0);
             this.my.sprite.potato.setVelocityY(0);
             this.my.sprite.potato.play('idle',true);
         }
+        //LAUNCH
     }
 }
-
